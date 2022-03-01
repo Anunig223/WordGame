ff5600667b257d646920debc3a909bddf9f223d0
Anunig223 <79616960+Anunig223@users.noreply.github.com>
2022-02-28 16:48:35 -0800

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function addUser(){
+    player_1_name = document.getElementById("player_1_name").value;
+    player_2_name = document.getElementById("player_2_name").value;
+    localStorage.setItem("player_1", player_1_name);
+    localStorage.setItem("player_2", player_2_name);
+
+    window.location = "game_page.html"
+}
